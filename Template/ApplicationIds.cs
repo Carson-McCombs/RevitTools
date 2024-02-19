2db7b76ec4d49b938664ab2520efb32f3ce5ab76
Carson-McCombs <carson.mccombs.work@gmail.com>
2024-02-18 18:48:11 -0600

    Removed some old code within the ThisApplication class. Commented the Generic RegisterDockablePane class. I added some comments to ApplicationIds class and allowed a new GUID to be generated for a specific type if there doesn't already exist one within the guidsTypeMap dictionary. Revert "Removed some old code within the ThisApplication class and

--- Template/ApplicationIds.cs
+++ Template/ApplicationIds.cs
@@ -1,5 +1,4 @@
-﻿using Autodesk.Revit.UI;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,46 +6,44 @@ using System.Threading.Tasks;
 
 namespace CarsonsAddins
 {
-    /*
-     * This class is used to store IDs for UI ( such as windows and dockable panes ) and for updaters, all of which are required to be registered through Revit
-     * 
-     * Originally, this class just stored IDs in the form of static/const variables. But after the the usuage of ComponentStates began, retrieving IDs by type became necessary to retrieve ( 
-     */
     public static class ApplicationIds
     {
-
+        /*public static Guid preferenceWindowId;
+        public static Guid pipingUpdaterId;
+        public static Guid pipingConnectionsUpdaterId;
+        public static Guid pipeEndPrepWindowId;
+        public static Guid simpleFilterPaneId;
+        public static Guid complexFilterPaneId;*/
         private static Dictionary<Type, Guid> guidsTypeMap; 
         public static void Init()
         {
-            guidsTypeMap = new Dictionary<Type, Guid>
-            {
-                { typeof(PreferenceWindow), new Guid("{1524F3C0-B6EB-4A32-985F-E3A7071F32B2}") },
-                { typeof(PipingLCUpdater), new Guid("{BD82E5B2-D984-49F9-8442-ED572680656D}") },
-                { typeof(PipingEndPrepUpdater), new Guid("{892AC63B-A827-466F-A8EA-F4418C27B7C2}") },
-                { typeof(PipeEndPrepWindow), new Guid("{7FAA23DB-AAFD-4F47-857E-C06D21B2717E}") },
-                { typeof(SimpleFilterDockablePane), new Guid("{5539CADC-1545-4427-9495-3E74FAB6071C}") },
-                { typeof(ComplexFilterDockablePane), new Guid("{DEF29C25-F7BA-4BE6-A9CD-B1702328043E}") }
-            };
+            guidsTypeMap = new Dictionary<Type, Guid>();
+            guidsTypeMap.Add(typeof(PreferenceWindow), new Guid("{1524F3C0-B6EB-4A32-985F-E3A7071F32B2}"));
+            guidsTypeMap.Add(typeof(PipingLCUpdater), new Guid("{BD82E5B2-D984-49F9-8442-ED572680656D}"));
+            guidsTypeMap.Add(typeof(PipingEndPrepUpdater), new Guid("{892AC63B-A827-466F-A8EA-F4418C27B7C2}"));
+            guidsTypeMap.Add(typeof(PipeEndPrepWindow), new Guid("{7FAA23DB-AAFD-4F47-857E-C06D21B2717E}"));
+            guidsTypeMap.Add(typeof(SimpleFilterDockablePane), new Guid("{5539CADC-1545-4427-9495-3E74FAB6071C}"));
+            guidsTypeMap.Add(typeof(ComplexFilterDockablePane), new Guid("{DEF29C25-F7BA-4BE6-A9CD-B1702328043E}"));
+            /*preferenceWindowId = new Guid("{1524F3C0-B6EB-4A32-985F-E3A7071F32B2}");
+            pipingUpdaterId = new Guid("{BD82E5B2-D984-49F9-8442-ED572680656D}");
+            pipingConnectionsUpdaterId = new Guid("{892AC63B-A827-466F-A8EA-F4418C27B7C2}");
+            pipeEndPrepWindowId = new Guid("{7FAA23DB-AAFD-4F47-857E-C06D21B2717E}");
+            simpleFilterPaneId = new Guid("{5539CADC-1545-4427-9495-3E74FAB6071C}");
+            complexFilterPaneId = new Guid("{DEF29C25-F7BA-4BE6-A9CD-B1702328043E}");*/
 
         }
         public static Guid GetId<T>()
         {
             Type t = typeof(T);
             if (guidsTypeMap.ContainsKey(t)) return guidsTypeMap[t];
-            return AddNewGuidForType(t);
+
+            return Guid.Empty;
         }
         public static Guid GetId(Type t)
         {
             if (guidsTypeMap.ContainsKey(t)) return guidsTypeMap[t];
-            return AddNewGuidForType(t);
 
-        }
-        private static Guid AddNewGuidForType(Type t)
-        {
-            Guid guid = Guid.NewGuid();
-            guidsTypeMap.Add(t, guid);
-            TaskDialog.Show("Unable to Find GUID for " + t.Name + ", generating new ID.", "Please add a GUID for the specified type within the guidsTypeMap in the ApplicationIds class.");
-            return guid;
+            return Guid.Empty;
         }
     }
 }
